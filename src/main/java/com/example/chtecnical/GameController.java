b9e1e712268a24a8ecc90c6ad0d2378feeefede9
Emma Brady <Emma.Brady12@hotmail.co.uk>
2020-05-15 19:20:59 +0100

    Added basic path to output provided games.json

--- src/main/java/com/example/chtecnical/GameController.java
+++ src/main/java/com/example/chtecnical/GameController.java
@@ -1,8 +1,13 @@
 package com.example.chtecnical;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 public class GameController {
@@ -12,4 +17,18 @@ public class GameController {
         return "Hello world!";
     }
 
+    @RequestMapping("/games")
+    private String getGameFile() throws IOException {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:games.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //Read File Content
+        String content = new String(Files.readAllBytes(file.toPath()));
+
+        return content;
+    }
 }
