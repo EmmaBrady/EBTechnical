c296971e952e873f69618d4992322c08539b61e5
Emma Brady <Emma.Brady12@hotmail.co.uk>
2020-05-15 16:20:55 +0100

    Initial commit of empty project

---
+++ src/main/java/com/example/chtecnical/ChtecnicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.chtecnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChtecnicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChtecnicalApplication.class, args);
+    }
+
+}
